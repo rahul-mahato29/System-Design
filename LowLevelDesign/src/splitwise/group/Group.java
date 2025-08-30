086b1f9b04d072982f83e4cf422b3fd612ad8577
Rahul Mahato <mahatorahul2901@gmail.com>
2025-08-30 22:40:42 +0530

    feat: add Group entity and GroupController for managing groups

--- LowLevelDesign/src/splitwise/group/Group.java
+++ LowLevelDesign/src/splitwise/group/Group.java
@@ -1,4 +1,53 @@
 package splitwise.group;
 
+import splitwise.expense.enums.ExpenseSplitType;
+import splitwise.expense.split.Split;
+import splitwise.user.User;
+import splitwise.entities.Expense;
+import splitwise.controllers.ExpenseController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    String groupId;
+    String groupName;
+    List<User> groupMembers;
+
+    List<Expense> expenseList;
+
+    ExpenseController expenseController;  //so that I can create expense through group also
+
+    Group(){
+        groupMembers = new ArrayList<>();
+        expenseList = new ArrayList<>();
+        expenseController = new ExpenseController();
+    }
+
+    //add member to group
+    public void addMember(User member){
+        groupMembers.add(member);
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public Expense createExpense(String expenseId, String description, double expenseAmount,
+                                 List<Split> splitDetails, ExpenseSplitType splitType, User paidByUser) {
+
+        Expense expense = expenseController.createExpense(expenseId, description, expenseAmount, paidByUser, splitType, splitDetails);
+        expenseList.add(expense);
+        return expense;
+    }
+
 }
