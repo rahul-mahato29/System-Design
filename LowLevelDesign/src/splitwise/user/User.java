d55851d7c4ee8676ef6bf64ae992efbb8732300e
Rahul Mahato <mahatorahul2901@gmail.com>
2025-08-30 22:40:38 +0530

    feat: add User entity and UserController for managing users

--- LowLevelDesign/src/splitwise/user/User.java
+++ LowLevelDesign/src/splitwise/user/User.java
@@ -1,4 +1,16 @@
 package splitwise.user;
 
 public class User {
+
+    String userId;
+    String name;
+
+    public User(String name, String userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
--- LowLevelDesign/src/splitwise/user/UserController.java
+++ LowLevelDesign/src/splitwise/user/UserController.java
@@ -1,4 +1,35 @@
 package splitwise.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserController {
+
+    List<User> userList;
+
+    public UserController() {
+        userList = new ArrayList<>();
+    }
+
+    //add-user
+    public void addUser(User user) {
+        userList.add(user);
+    }
+
+    //getUserById
+    public User getUserById(String userID) {
+        for (User user : userList) {
+            if (user.getUserId().equals(userID)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    //getAllUser
+    public List<User> getAllUsers(){
+        return userList;
+    }
+
+
 }
