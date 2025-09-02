831b7193b960e994cef903d8e5a5be50847660ce
Rahul Mahato <mahatorahul2901@gmail.com>
2025-09-03 00:34:15 +0530

    LRU-Cache

--- LowLevelDesign/src/LRUCache/Main.java
+++ LowLevelDesign/src/LRUCache/Main.java
@@ -1,4 +1,8 @@
 package LRUCache;
 
 public class Main {
+    public static void main(String[] args) {
+        LRUCacheDemo lruCacheDemo = new LRUCacheDemo();
+        lruCacheDemo.run();
+    }
 }
--- LowLevelDesign/src/LRUCache/Node.java
+++ LowLevelDesign/src/LRUCache/Node.java
@@ -1,4 +1,12 @@
 package LRUCache;
 
-public class Node {
+class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> prev, next;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
 }
